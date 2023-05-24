36d6189b7ae124e4ba2da03992e5a6b495a178b1
Yunus Emre Baş <emrebasyunus1@gmail.com>
2023-05-24 18:10:44 +0300

    Bu Derste Tamamdır.

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        double c;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("1. Kenarı Giriniz : ");
+        a = inp.nextInt();
+        System.out.print("2. Kenarı Giriniz : ");
+        b = inp.nextInt();
+
+        c=Math.sqrt((a*a)+(b*b));
+        System.out.println("Hipotenüs = " + c);
+
+        double cevre = (a+b+c);
+        double u = cevre / 2;
+        double alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+
+        System.out.println("Üçgenin Cevresi : " + cevre);
+        System.out.println("Üçgenin Alanı : " + alan);
+
+
+    }
+}
